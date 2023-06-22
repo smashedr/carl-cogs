0d5174007ab36012674a4b801cd33a2dccad0279
Shane <6071159+smashedr@users.noreply.github.com>
2023-06-22 01:44:14 -0700

    Phase 6 - weather: adding what i tell you what

--- weather/weather.py
+++ weather/weather.py
@@ -50,42 +50,51 @@ class Weather(commands.Cog):
         geo = self.gl.geocode(location)
         if not geo or not geo.latitude or not geo.longitude:
             return await ctx.send(f'⛔  Error getting Lat/Lon Data for: {location}')
-        weather: Dict[str, Any] = await self.get_weather(geo.latitude, geo.longitude)
+        weather, forecast = await self.get_weather(geo.latitude, geo.longitude)
         if not weather or not weather['features']:
             return await ctx.send(f'⛔  Error getting Weather for: {location}\n'
                                   f'Lat: `{geo.latitude}` Lon: `{geo.longitude}`')
-        log.debug('-'*40)
-        log.debug(weather)
-        log.debug('-'*40)
+        # log.debug('-'*40)
+        # log.debug(weather)
+        # log.debug('-'*40)
         # tz = self.tf.timezone_at(lat=geo.latitude, lng=geo.longitude)
         # timezone = pytz.timezone(tz)
         # current_time_utc = datetime.datetime.now(pytz.UTC)
         # current_time = current_time_utc.astimezone(timezone)
-        embed = self.gen_embed(geo, weather['features'][0]['properties'])
+        embed = self.gen_embed(geo, weather['features'][0]['properties'],
+                               forecast['properties']['periods'][0])
         await ctx.send(embed=embed)
 
-    def gen_embed(self, geo: geopy.location.Location, weather: Dict[str, Any]) -> discord.Embed:
+    def gen_embed(self, geo: geopy.location.Location, weather: Dict[str, Any],
+                  period: Dict[str, Any]) -> discord.Embed:
         embed = discord.Embed(
             title=geo.raw['display_name'],
             url=self.url.format(lat=geo.latitude, lon=geo.longitude),
-            description=weather['textDescription'] or None,
             timestamp=datetime.datetime.now(),
         )
         embed.set_author(name=f'Lat: {geo.latitude} / Lon: {geo.longitude}')
         if weather['icon']:
             embed.set_thumbnail(url=weather['icon'])
 
+        description = ''
+        if weather['textDescription']:
+            description += f"Currently {weather['textDescription']}. "
+        description += f"{period['name']}"
+        if period['detailedForecast']:
+            description += f", {period['detailedForecast']}"
+        embed.description = description
+
         if weather['temperature']['value']:
             temp = round((weather['temperature']['value'] * 9/5) + 32, 1)
         else:
             temp = 'Unknown'
-        embed.add_field(name='Temperature', value=f"{temp} F")
+        embed.add_field(name='Temperature', value=f"{temp} °F")
 
         if weather['dewpoint']['value']:
             dew = round((weather['dewpoint']['value'] * 9/5) + 32, 1)
         else:
             dew = 'Unknown'
-        embed.add_field(name='Dew Point', value=f"{dew} F")
+        embed.add_field(name='Dew Point', value=f"{dew} °F")
 
         if weather['relativeHumidity']['value']:
             humi = round(weather['relativeHumidity']['value'], 1)
@@ -97,27 +106,27 @@ class Weather(commands.Cog):
         embed.add_field(name='Wind Speed', value=f"{speed} km/h")
         gust = weather['windGust']['value'] or 0
         embed.add_field(name='Wind Gust', value=f"{gust} km/h")
-        direction = weather['windDirection']['value'] or 'Still'
-        embed.add_field(name='Wind Direction', value=f"{direction}")
+        direction = weather['windDirection']['value'] or 'N/A'
+        embed.add_field(name='Wind Direction', value=f"{direction}°")
 
         if weather['elevation']['value']:
             elevation = round(weather['elevation']['value'] * 3.28084, 1)
-            embed.add_field(name='Elevation', value=f"{elevation} Ft")
+            embed.add_field(name='Elevation', value=f"{elevation} ft")
 
         if weather['barometricPressure']['value']:
-            pressure = weather['barometricPressure']['value']
-            embed.add_field(name='Pressure', value=f"{pressure} Pa")
+            pressure = round(weather['barometricPressure']['value'] / 3386.39, 2)
+            embed.add_field(name='Pressure', value=f"{pressure} inHg")
 
         tz = self.tf.timezone_at(lat=geo.latitude, lng=geo.longitude)
         embed.add_field(name='Time Zone', value=f"{tz}")
         return embed
 
-    async def get_weather(self, lat: float, lon: float) -> Dict[str, Any]:
+    async def get_weather(self, lat: float, lon: float) -> Tuple[dict, dict]:
         location_url = f'https://api.weather.gov/points/{lat},{lon}'
         location_data: Dict[str, Any] = await self._get_json(location_url)
 
-        # forecast_url: str = location_data["properties"]["forecast"]
-        # forecast: Dict[str, Any] = await self._get_json(forecast_url)
+        forecast_url: str = location_data["properties"]["forecast"]
+        forecast: Dict[str, Any] = await self._get_json(forecast_url)
 
         stations_url: str = location_data["properties"]["observationStations"]
         stations: Dict[str, Any] = await self._get_json(stations_url)
@@ -126,16 +135,7 @@ class Weather(commands.Cog):
         observation_url: str = stations["features"][0]["id"] + f'/observations'
         observation: Dict[str, Any] = await self._get_json(observation_url)
 
-        return observation
-
-    async def get_forecast(self, lat: float, lon: float) -> Dict[str, Any]:
-        location_url = f'https://api.weather.gov/points/{lat},{lon}'
-        location_data: Dict[str, Any] = await self._get_json(location_url)
-
-        forecast_url: str = location_data["properties"]["forecast"]
-        forecast: Dict[str, Any] = await self._get_json(forecast_url)
-
-        return forecast
+        return observation, forecast
 
     async def _get_json(self, url: str, **kwargs) -> Dict[str, Any]:
         log.debug('url: %s', url)
@@ -144,6 +144,15 @@ class Weather(commands.Cog):
             r.raise_for_status()
         return r.json()
 
+    # async def get_forecast(self, lat: float, lon: float) -> Dict[str, Any]:
+    #     location_url = f'https://api.weather.gov/points/{lat},{lon}'
+    #     location_data: Dict[str, Any] = await self._get_json(location_url)
+    #
+    #     forecast_url: str = location_data["properties"]["forecast"]
+    #     forecast: Dict[str, Any] = await self._get_json(forecast_url)
+    #
+    #     return forecast
+    #
     # def gen_forecast_embed(self, geo: geopy.location.Location, period: Dict[str, Any]) -> discord.Embed:
     #     embed = discord.Embed(
     #         title=geo.raw['display_name'],
