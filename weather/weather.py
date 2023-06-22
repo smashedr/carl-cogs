1bed579814d29db44f14b4a1c6ea928f81f1bc36
Shane <6071159+smashedr@users.noreply.github.com>
2023-06-22 12:45:57 -0700

    Phase 6 - weather: major update, FIRE

--- weather/weather.py
+++ weather/weather.py
@@ -47,23 +47,21 @@ class Weather(commands.Cog):
     @app_commands.describe(location='Location to get Weather for')
     async def weather_command(self, ctx: commands.Context, *, location: str):
         """Get Weather for <location>"""
-        geo = self.gl.geocode(location)
-        if not geo or not geo.latitude or not geo.longitude:
-            return await ctx.send(f'⛔  Error getting Lat/Lon Data for: {location}')
-        weather, forecast = await self.get_weather(geo.latitude, geo.longitude)
-        if not weather or not weather['features']:
-            return await ctx.send(f'⛔  Error getting Weather for: {location}\n'
-                                  f'Lat: `{geo.latitude}` Lon: `{geo.longitude}`')
-        # log.debug('-'*40)
-        # log.debug(weather)
-        # log.debug('-'*40)
-        # tz = self.tf.timezone_at(lat=geo.latitude, lng=geo.longitude)
-        # timezone = pytz.timezone(tz)
-        # current_time_utc = datetime.datetime.now(pytz.UTC)
-        # current_time = current_time_utc.astimezone(timezone)
-        embed = self.gen_embed(geo, weather['features'][0]['properties'],
-                               forecast['properties']['periods'][0])
-        await ctx.send(embed=embed)
+        async with ctx.typing():
+            geo = self.gl.geocode(location)
+            if not geo or not geo.latitude or not geo.longitude:
+                return await ctx.send(f'⛔  Error getting Lat/Lon Data for: {location}')
+            weather, forecast = await self.get_weather(geo.latitude, geo.longitude)
+            if not weather or not weather['features']:
+                return await ctx.send(f'⛔  Error getting Weather for: {location}\n'
+                                      f'Lat: `{geo.latitude}` Lon: `{geo.longitude}`')
+            # tz = self.tf.timezone_at(lat=geo.latitude, lng=geo.longitude)
+            # timezone = pytz.timezone(tz)
+            # current_time_utc = datetime.datetime.now(pytz.UTC)
+            # current_time = current_time_utc.astimezone(timezone)
+            embed = self.gen_embed(geo, weather['features'][0]['properties'],
+                                   forecast['properties']['periods'][0])
+            await ctx.send(embed=embed)
 
     def gen_embed(self, geo: geopy.location.Location, weather: Dict[str, Any],
                   period: Dict[str, Any]) -> discord.Embed:
@@ -72,7 +70,10 @@ class Weather(commands.Cog):
             url=self.url.format(lat=geo.latitude, lon=geo.longitude),
             timestamp=datetime.datetime.now(),
         )
-        embed.set_author(name=f'Lat: {geo.latitude} / Lon: {geo.longitude}')
+        embed.set_author(
+            name=f'Lat: {geo.latitude} / Lon: {geo.longitude}',
+            url=self.geohack_url_from_geo(geo),
+        )
         if weather['icon']:
             embed.set_thumbnail(url=weather['icon'])
 
@@ -85,40 +86,71 @@ class Weather(commands.Cog):
         embed.description = description
 
         if weather['temperature']['value']:
-            temp = round((weather['temperature']['value'] * 9/5) + 32, 1)
+            temp = self._num((weather['temperature']['value'] * 9/5) + 32, to=1)
+            temp_s = temp + ' °F'
         else:
-            temp = 'Unknown'
-        embed.add_field(name='Temperature', value=f"{temp} °F")
+            temp = None
+            temp_s = 'N/A'
+        embed.add_field(name='Temperature', value=f"{temp_s}")
 
         if weather['dewpoint']['value']:
-            dew = round((weather['dewpoint']['value'] * 9/5) + 32, 1)
+            dew = self._num((weather['dewpoint']['value'] * 9/5) + 32, to=1, suffix=' °F')
         else:
-            dew = 'Unknown'
-        embed.add_field(name='Dew Point', value=f"{dew} °F")
+            dew = 'N/A'
+        embed.add_field(name='Dew Point', value=f"{dew}")
 
         if weather['relativeHumidity']['value']:
-            humi = round(weather['relativeHumidity']['value'], 1)
+            humi = self._num(weather['relativeHumidity']['value'], to=1, suffix=' %')
         else:
-            humi = 'Unknown'
-        embed.add_field(name='Humidity', value=f"{humi} %")
+            humi = 'N/A'
+        embed.add_field(name='Humidity', value=f"{humi}")
 
-        speed = weather['windSpeed']['value'] or 0
-        embed.add_field(name='Wind Speed', value=f"{speed} km/h")
-        gust = weather['windGust']['value'] or 0
-        embed.add_field(name='Wind Gust', value=f"{gust} km/h")
-        direction = weather['windDirection']['value'] or 'N/A'
-        embed.add_field(name='Wind Direction', value=f"{direction}°")
+        if weather['windSpeed']['value']:
+            speed = self._num(weather['windSpeed']['value'] / 1.609344, to=1, suffix=' mph')
+        else:
+            speed = 'N/A'
+        embed.add_field(name='Wind Speed', value=f"{speed}")
+
+        if weather['windGust']['value']:
+            gust = self._num(weather['windGust']['value'] / 1.609344, to=1, suffix=' mph')
+        else:
+            gust = 'N/A'
+        embed.add_field(name='Wind Gust', value=f"{gust}")
+
+        direction = str(weather['windDirection']['value']) or 'N/A'
+        direction += '°' if weather['windDirection']['value'] else ''
+        embed.add_field(name='Wind Direction', value=f"{direction}")
 
         if weather['elevation']['value']:
-            elevation = round(weather['elevation']['value'] * 3.28084, 1)
-            embed.add_field(name='Elevation', value=f"{elevation} ft")
+            elevation = self._num(weather['elevation']['value'] * 3.28084, suffix=' ft')
+            embed.add_field(name='Elevation', value=f"{elevation}")
 
         if weather['barometricPressure']['value']:
-            pressure = round(weather['barometricPressure']['value'] / 3386.39, 2)
+            # pressure = self._num(weather['barometricPressure']['value'] / 3386.39, to=2, suffix=' inHg')
+            pressure = format(weather['barometricPressure']['value'] / 3386.39, '.2f')
             embed.add_field(name='Pressure', value=f"{pressure} inHg")
 
         tz = self.tf.timezone_at(lat=geo.latitude, lng=geo.longitude)
         embed.add_field(name='Time Zone', value=f"{tz}")
+
+        if temp:
+            temp_f = float(temp)
+            if temp_f < 0:
+                embed.colour = discord.Colour.purple()
+            elif 0 <= temp_f < 32:
+                embed.colour = discord.Colour.blue()
+            elif 32 <= temp_f < 65:
+                embed.colour = discord.Colour.dark_teal()
+            elif 65 <= temp_f < 75:
+                embed.colour = discord.Colour.green()
+            elif 75 <= temp_f < 85:
+                embed.colour = discord.Colour.yellow()
+            elif 85 <= temp_f < 95:
+                embed.colour = discord.Colour.orange()
+            elif 95 <= temp_f:
+                embed.colour = discord.Colour.red()
+        else:
+            embed.colour = discord.Colour.light_gray()
         return embed
 
     async def get_weather(self, lat: float, lon: float) -> Tuple[dict, dict]:
@@ -143,6 +175,28 @@ class Weather(commands.Cog):
             r = await client.get(url, params=kwargs)
             r.raise_for_status()
         return r.json()
+
+    def geohack_url_from_geo(self, geo: geopy.location.Location, name: Optional[str] = None) -> str:
+        name = name.replace(' ', '_') if name else geo.raw['display_name'].replace(' ', '_')
+        dn, mn, sn = self.dd2dms(geo.latitude)
+        dw, mw, sw = self.dd2dms(geo.longitude)
+        params = f'{dn}_{mn}_{sn}_N_{dw}_{mw}_{sw}_W_scale:500000'
+        return f'https://geohack.toolforge.org/geohack.php?pagename={name}&params={params}'
+
+    @staticmethod
+    def dd2dms(dd: Union[float, int]) -> Tuple[int, int, int]:
+        """DD to DMS"""
+        mult = -1 if dd < 0 else 1
+        mnt, sec = divmod(abs(dd)*3600, 60)
+        deg, mnt = divmod(mnt, 60)
+        return abs(mult*deg), abs(mult*mnt), abs(mult*sec)
+
+    @staticmethod
+    def _num(number: Union[float, int], to: Optional[int] = 0,
+             prefix: Optional[str] = '', suffix: Optional[str] = '') -> str:
+        """Remove Trailing Zeros and Decimal with Prefix and Suffix"""
+        digit = str(round(number, to)).rstrip('0').rstrip('.')
+        return prefix + digit + suffix
 
     # async def get_forecast(self, lat: float, lon: float) -> Dict[str, Any]:
     #     location_url = f'https://api.weather.gov/points/{lat},{lon}'
